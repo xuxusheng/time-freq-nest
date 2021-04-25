9fd0534d037c85a44056e8533378b057d7789bd2
许盛 <20691718@qq.com>
2021-04-25 10:32:14 +0800

    feat: 加入 user 模块、accessLog 中间件

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { AccessLogMiddleware } from './common/middleware/access-log.middleware';
 
 @Module({
-  imports: [],
+  imports: [UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): any {
+    consumer.apply(AccessLogMiddleware).forRoutes('*');
+  }
+}
